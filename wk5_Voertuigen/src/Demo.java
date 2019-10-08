ebf050c20bdb6157c89ba20a971db8096d482634
hanbioinformatica <martijn.van.der.bruggen@gmail.com>
2019-10-08 16:16:22 +0200

    adding old files

---
+++ wk5_Voertuigen/src/Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+    static int getal = 7;
+
+    public static void main(String[] args) {
+        System.out.print(getal);
+    }
+
+}
---
+++ wk5_Voertuigen/src/Fiets.java
@@ -0,0 +1,15 @@
+public class Fiets extends Voertuig {
+
+    public Fiets() {
+        setMaxSnelheid(50);
+    }
+
+    public int getSnelheid() {
+
+        return super.getSnelheid();
+    }
+
+    public void setSnelheid(int snelheid) {
+
+    }
+}

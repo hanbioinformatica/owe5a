c5b97a276cc158070e4a51ce68408823cb2e6960
martijn <Martijn.vanderBruggen@han.nl>
2021-09-07 11:52:15 +0200

    Start 2122 - na les 1

--- src/src/HelloWorld.java
+++ src/src/HelloWorld.java
@@ -1,8 +1,10 @@
+package src;
+
+import javax.swing.*;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-        String s1 = "ATGGAGCACCATGGA";
-        boolean b = s1.matches("[ATGC]*");
-        System.out.println(b);
+
     }
 }
---
+++ wk2_variabelen/src/VarDemo.java
@@ -0,0 +1,13 @@
+public class VarDemo {
+
+    public static final double PI = 3.1415;
+    public static final boolean WAAR = true;
+
+    public static void main(String[] args) {
+        String een = " Hello";
+        System.out.println("Hello World!".toUpperCase().replace("L","C").toLowerCase());
+
+
+    }
+
+}

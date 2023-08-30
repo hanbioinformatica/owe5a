7cae11403128977c480b956eeadcd502570e5b8e
Martijn <Martijn.vanderBruggen@han.nl>
2023-08-30 21:58:07 +0200

    Start - Updating minor fixes

---
+++ demoGUI/src/Dialog.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Dialog {
+
+    public static void main(String[] args) {
+        String s = JOptionPane.showInputDialog("Wat wil je?");
+        System.out.println(s);
+    }
+
+}
